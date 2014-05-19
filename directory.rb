84dabd69b22d441e4ec81d45d31fd9853749d550
Faezrah Rizalman <fwrizalman@gmail.com>
2014-05-19 14:15:01 +0100

    loading the data from ARGV

--- directory.rb
+++ directory.rb
@@ -15,27 +15,27 @@ def print_footer
   puts "Overall, we have #{@students.length} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # get the first name
+  name = STDIN.gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do    
+    # add the student hash to the array
+    @students << {:name => name, :cohort => :may}    
+    puts "Now we have #{@students.length} students"
+    # get another name from the user
+    name = STDIN.gets.chomp
+  end
+end
+
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
   puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
-end
-
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # get the first name
-  name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do    
-    # add the student hash to the array
-    @students << {:name => name, :cohort => :november}    
-    puts "Now we have #{@students.length} students"
-    # get another name from the user
-    name = gets.chomp
-  end
 end
 
 def show_students
@@ -56,13 +56,25 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
       @students << {:name => name, :cohort => cohort.to_sym}
   end
   file.close
+end
+
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+    puts "Loaded #{@students.length} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
 end
 
 def process(selection)
@@ -85,8 +97,9 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
+try_load_students
 interactive_menu
